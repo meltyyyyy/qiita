befa87b7c7024b9b379c3dbad4fbc09011c15302
takeru.abe <takeru.abe@gaiax.com>
2022-07-03 14:08:52 +0900

    update gpr.py

--- bo/gpr.py
+++ bo/gpr.py
@@ -6,12 +6,27 @@ such that every finite collection of those random variables has a multivariate n
 """
 
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-pastel')
+from kernels import rbf, periodic, exp, linear
 
 
 def objective(x):
-    return 2 * np.sin(x) + 3 * np.cos(2 * x) + 5 * \
-        np.sin(2 / 3 * x) + np.random.randn(len(x))
+    return 2 * np.sin(x) + 3 * np.cos(2 * x) + 5 * np.sin(2 / 3 * x)
+
+
+def train_test_split(x, y, test_size):
+    assert len(x) == len(y)
+    n_samples = len(x)
+    test_indices = np.sort(
+        np.random.choice(
+            np.arange(n), int(
+                n_samples * test_size), replace=False))
+    train_indices = np.ones(n_samples, dtype=bool)
+    train_indices[test_indices] = False
+    test_indices = ~ train_indices
+
+    return x[train_indices], x[test_indices], y[train_indices], y[test_indices]
 
 
 n = 100
@@ -19,109 +34,77 @@ data_x = np.linspace(0, 4 * np.pi, n)
 data_y = objective(data_x)
 
 
-missing_rate = 0.2
-sample_idx = np.sort(
-    np.random.choice(
-        np.arange(n), int(
-            n * missing_rate), replace=False))
+x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.70)
 
 
-def kernel(x, x_prime, p, q, r):
-    """Kernel Function
+def plot_gpr(x_train, y_train, x_test, mu, var):
+    plt.figure(figsize=(16, 8))
+    plt.title('Gaussian Process Regressor', fontsize=20)
 
-    Args:
-        x (float): data
-        x_prime (float): data
-        p (float): hyper parameter
-        q (float): hyper parameter
-        r (float): error
+    plt.plot(data_x, data_y, label='objective')
+    plt.plot(
+        x_train,
+        y_train,
+        'o',
+        label='train data')
 
-    """
-    if x == x_prime:
-        delta = 1
-    else:
-        delta = 0
+    std = np.sqrt(np.abs(var))
 
-    return p * np.exp(-1 * (x - x_prime)**2 / q) + (r * delta)
+    plt.plot(x_test, mu, label='mean')
 
+    plt.fill_between(
+        x_test,
+        mu + 2 * std,
+        mu - 2 * std,
+        alpha=.2,
+        label='standard deviation')
+    plt.legend(
+        loc='lower left',
+        fontsize=12)
 
-# training data
-x_train = np.copy(data_x[sample_idx])
-y_train = np.copy(data_y[sample_idx])
-
-# test data
-x_test = np.copy(data_x)
-
-# average
-mu = []
-# variance
-var = []
-
-# hyper parameters
-Theta_1 = 1.0
-Theta_2 = 0.4
-Theta_3 = 0.1
-
-train_length = len(x_train)
-test_length = len(x_test)
+    plt.savefig('gpr.png')
 
 
-K = np.zeros((train_length, train_length))
-for x_idx in range(train_length):
-    for x_prime_idx in range(train_length):
-        K[x_idx, x_prime_idx] = kernel(
-            x=x_idx, x_prime=x_prime_idx, p=Theta_1, q=Theta_2, r=Theta_3)
+def gpr(x_train, y_train, x_test, kernel):
+    # average
+    mu = []
+    # variance
+    var = []
 
-yy = np.dot(np.linalg.inv(K), y_train)
+    train_length = len(x_train)
+    test_length = len(x_test)
 
-for x_test_idx in range(test_length):
-    k = np.zeros((train_length,))
-
+    K = np.zeros((train_length, train_length))
     for x_idx in range(train_length):
-        k[x_idx] = kernel(
-            x=x_train[x_idx],
-            x_prime=x_test[x_test_idx],
-            p=Theta_1,
-            q=Theta_2,
-            r=Theta_3)
+        for x_prime_idx in range(train_length):
+            K[x_idx, x_prime_idx] = kernel(x_train[x_idx], x_train[x_prime_idx])
 
-    s = kernel(
-        x=x_test[x_test_idx],
-        x_prime=x_test[x_test_idx],
-        p=Theta_1,
-        q=Theta_2,
-        r=Theta_3)
-    mu.append(np.dot(k, yy))
-    kK_ = np.dot(k, np.linalg.inv(K))
-    var.append(s - np.dot(kK_, k.T))
+    yy = np.dot(np.linalg.inv(K), y_train)
 
-plt.figure(figsize=(16, 8))
-plt.title('Signal prediction by Gaussian process', fontsize=20)
+    for x_test_idx in range(test_length):
+        k = np.zeros((train_length,))
+        for x_idx in range(train_length):
+            k[x_idx] = kernel(
+                x_train[x_idx],
+                x_test[x_test_idx])
+        s = kernel(
+            x_test[x_test_idx],
+            x_test[x_test_idx])
+        mu.append(np.dot(k, yy))
+        kK_ = np.dot(k, np.linalg.inv(K))
+        var.append(s - np.dot(kK_, k.T))
+    return mu, var
 
-plt.plot(data_x, data_y, 'x', color='orange', label='correct signal')
-plt.plot(
-    data_x[sample_idx],
-    data_y[sample_idx],
-    'o',
-    color='red',
-    label='sample dots')
 
-std = np.sqrt(np.abs(var))
+# Radiant Basis Kernel
+kernel = lambda x, x_prime : rbf(x, x_prime, theta_1=1.0, theta_2=1.0)
+# # Periodic Kernel
+# kernel = lambda x, x_prime : periodic(x, x_prime, theta_1=1.0, theta_2=1.0)
+# # Exponential Kernel
+# kernel = lambda x, x_prime : exp(x, x_prime, theta=1.0)
+# # Radiant Basis Kernel
+# kernel = lambda x, x_prime : linear(x, x_prime, theta=1.0)
 
-plt.plot(x_test, mu, color='red', label='Mean by Gaussian process')
-plt.fill_between(
-    x_test,
-    mu + 2 * std,
-    mu - 2 * std,
-    alpha=.2,
-    color='pink',
-    label='Standard deviation by Gaussian process')
 
-plt.legend(
-    bbox_to_anchor=(
-        1.05,
-        1),
-    loc='upper left',
-    borderaxespad=0,
-    fontsize=12)
-plt.savefig('gpr.png')
+mu, var = gpr(x_train, y_train, x_test, kernel)
+plot_gpr(x_train, y_train, x_test, mu, var)
