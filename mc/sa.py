345fb8d0827796d81cd5683de4570b403bc1d9be
takeru.abe <takeru.abe@gaiax.com>
2022-07-09 14:39:53 +0900

    add comment

---
+++ mc/sa.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Simulated Annealing
+This is Simulated Annealing implementation.
+Simulated Annealing (SA) is a probabilistic technique
+for approximating the global optimum of a given function.
+"""
