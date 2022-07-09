13fbd6e82cdfdfd986f48c21ce2de61561f5377c
takeru.abe <takeru.abe@gaiax.com>
2022-07-09 16:03:24 +0900

    create sa

--- mc/sa.py
+++ mc/sa.py
@@ -29,12 +29,15 @@ plt.close()
 # plot temprature
 n_iter = 100
 rates = [0.90, 0.95, 0.99]
-fig, axes = plt.subplots(nrows=1, ncols=3, tight_layout=True, **{"figsize": (24, 8)})
+fig, axes = plt.subplots(
+    nrows=1, ncols=3, tight_layout=True, **{"figsize": (24, 8)})
 fig.suptitle('Cooling schedule', fontsize=18)
+
 for i, rate in enumerate(rates):
     temp = np.array([])
     T = 1.
     def cool(T): return rate * T
+
     for j in range(n_iter):
         T = cool(T)
         temp = np.append(temp, T)
@@ -43,27 +46,73 @@ for i, rate in enumerate(rates):
     axes[i].set_ylabel('Temprature')
     axes[i].set_xlim([-5, 105])
     axes[i].set_ylim([-0.1, 1.1])
-
+    axes[i].legend()
 fig.savefig('temprature.png')
 
 
 # metropolise criterion on each temprature
 n_iter = 100
-T = 1.
-def cool(T): return .99 * T
+rates = [0.90, 0.95, 0.99]
 
+fig, axes = plt.subplots(
+    nrows=1, ncols=3, tight_layout=True, **{"figsize": (24, 8)})
+fig.suptitle('Metropolise Criterion', fontsize=18)
 
-plt.figure()
-plt.title('Criteria', fontsize=14)
-for diff in [0.03, 0.1, 0.3, 1.0, 3.0]:
-    metro = np.array([])
+for i, rate in enumerate(rates):
+    T = 1.
+    def cool(T): return rate * T
+
+    for diff in [0.03, 0.1, 0.3, 1.0, 3.0]:
+        metro = np.array([])
+
+        for _ in range(n_iter):
+            T = cool(T)
+            criteion = np.exp(- diff / T)
+            metro = np.append(metro, criteion)
+        axes[i].plot(metro, label=f'diff: {diff}')
+    axes[i].set_xlabel('Iteration')
+    axes[i].set_ylabel('Metropolise criterion')
+    axes[i].legend()
+
+fig.savefig('metropolise.png')
+
+def simulated_annealinng(objective, cooling_rate, n_iter):
+    x_iter = np.zeros((n_iter, ))
+    obj_iter = np.zeros((n_iter, ))
+
+    # set initial temperature and cooling schedule
+    T = 1.
+    def cool(T): return cooling_rate * T
+
+    index = np.random.choice(n, 1)
+    curr_x = data_x[index]
+    curr_obj = objective(curr_x)
+
+    best_x = curr_x
+    best_obj = curr_obj
+
     for i in range(n_iter):
+
+        # decrease T according to cooling schedule
         T = cool(T)
-        criteion = np.exp(- diff / T)
-        metro = np.append(metro, criteion)
-    plt.plot(metro, label=f'{diff}')
-plt.xlabel('Iteration')
-plt.ylabel('Metropolise criterion')
-plt.legend()
-plt.savefig('metropolise.png')
-plt.close()
+
+        index = np.random.choice(n, 1)
+        new_x = data_x(index)
+        new_obj = objective(new_x)
+
+        # update current solution iterate
+        if (new_obj > curr_obj) or (np.random.rand()
+                                   < np.exp((new_obj - curr_obj) / T)):
+            curr_x = new_x
+            curr_obj = new_obj
+
+        # Update best solution
+        if new_obj > best_obj:
+            best_x = new_x
+            best_obj = new_obj
+
+        # save solution
+        x_iter[i] = best_x
+        obj_iter[i] = best_obj
+
+    return x_iter, obj_iter
