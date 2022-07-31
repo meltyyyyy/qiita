c131dee989e06a448bac005dcf1024c3f73db7d3
takeru.abe <takeru.abe@gaiax.com>
2022-07-31 13:59:46 +0900

    refactor

--- bo/gp/kernels.py
+++ bo/gp/kernels.py
@@ -3,32 +3,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from utils.plot import plot_kernel
 plt.style.use('seaborn-pastel')
-
-
-def plot_kernel(kernel, title):
-    n = 100
-    x = np.linspace(-10, 10, n)
-    ys = []
-
-    for i in range(3):
-        mkernel = np.zeros((x.shape[0], x.shape[0]))
-        for i_row in range(x.shape[0]):
-            for i_col in range(i_row, x.shape[0]):
-                mkernel[i_row, i_col] = kernel(x[i_row], x[i_col])
-                mkernel[i_col, i_row] = mkernel[i_row, i_col]
-
-        K = 1**2 * np.dot(mkernel, mkernel.T)
-        y = np.random.multivariate_normal(np.zeros(len(x)), K)
-        ys.append(y)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    for y in ys:
-        ax.plot(x, y)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title(f'{title}')
 
 
 def rbf(x, x_prime, theta_1, theta_2):
@@ -86,4 +62,5 @@ if __name__ == "__main__":
     plot_kernel(lambda x, x_prime: periodic(x, x_prime, 0.5, 0.5), 'Periodic Kernel')
     plot_kernel(lambda x, x_prime: linear(x, x_prime, 0.5), 'Linear Kernel')
     plot_kernel(lambda x, x_prime: rbf(x, x_prime, 0.5, 0.5), 'Exponential Kernel')
-    plot_kernel(lambda x, x_prime: 0.8 * exp(x, x_prime, 0.5) + 0.2 * periodic(x, x_prime, 0.5, 0.5), 'Exponential + Periodic Kernel')
+    plot_kernel(lambda x, x_prime: 0.8 * exp(x, x_prime, 0.5) + 0.2 *
+                periodic(x, x_prime, 0.5, 0.5), 'Exponential + Periodic Kernel')
--- bo/gp/utils/plot.py
+++ bo/gp/utils/plot.py
@@ -28,3 +28,28 @@ def plot_gpr(x, y, x_train, y_train, x_test, mu, var):
         fontsize=12)
 
     plt.savefig('gpr.png')
+
+def plot_kernel(kernel, title):
+    n = 100
+    x = np.linspace(-10, 10, n)
+    ys = []
+
+    for i in range(3):
+        mkernel = np.zeros((x.shape[0], x.shape[0]))
+        for i_row in range(x.shape[0]):
+            for i_col in range(i_row, x.shape[0]):
+                mkernel[i_row, i_col] = kernel(x[i_row], x[i_col])
+                mkernel[i_col, i_row] = mkernel[i_row, i_col]
+
+        K = 1**2 * np.dot(mkernel, mkernel.T)
+        y = np.random.multivariate_normal(np.zeros(len(x)), K)
+        ys.append(y)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for y in ys:
+        ax.plot(x, y)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title(f'{title}')
+    plt.savefig(f'{title}.png')
