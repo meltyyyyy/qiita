cc7576d57d57e701ecd6749c940048f3658c5145
takeru.abe <takeru.abe@gaiax.com>
2022-08-04 13:17:07 +0900

    update cauchy.py

--- bo/gp/likelihoods/cauchy.py
+++ bo/gp/likelihoods/cauchy.py
@@ -43,79 +43,57 @@ def kernel(x, x_prime, noise):
     return rbf(x, x_prime, theta_1=1.0, theta_2=1.0) + delta
 
 
-def plot_gpr(x, y, x_train, f_posterior):
+def plot_gpr(x, y, f_posterior):
     plt.figure(figsize=(16, 8))
     plt.title('Cauchy', fontsize=20)
     plt.plot(x, y, 'x', label='objective')
 
     for i in range(f_posterior.shape[1]):
-        plt.plot(x_train, f_posterior[:, i])
-    # plt.legend(
-    #     loc='lower left',
-    #     fontsize=12)
+        plt.plot(x, f_posterior[:, i])
     plt.savefig('gpr.png')
 
 
-def gpr(x_train, y_train, x_test, kernel, n_iter=250):
-    train_length = len(x_train)
-    test_length = len(x_test)
+def gpr(x, y, kernel, n_iter=100):
+    N = len(x)
 
-    K = np.zeros((train_length, train_length))
-    for x_idx in range(train_length):
-        for x_prime_idx in range(train_length):
+    K = np.zeros((N, N))
+    for x_idx in range(N):
+        for x_prime_idx in range(N):
             K[x_idx, x_prime_idx] = kernel(
-                x_train[x_idx], x_train[x_prime_idx], x_idx == x_prime_idx)
+                x[x_idx], x[x_prime_idx], x_idx == x_prime_idx)
 
     K_inv = np.linalg.inv(K)
     L_ = np.linalg.cholesky(K)
 
-    def log_marginal_likelihood(y, f, gamma=1.0):
+    # Normalization
+    y_mean = np.mean(y)
+    y_std = np.std(y)
+    y = (y - y_mean) / y_std
+
+    def log_marginal_likelihood(y, f, gamma=0.2):
         cauchy = - np.sum(np.log(gamma + (y - f)**2 / gamma))
         normal = - 0.5 * np.dot(f, np.dot(K_inv, f))
         return cauchy + normal
 
-    burn_in = 200
+    burn_in = 50
     n_samples = n_iter - burn_in
     assert n_iter > burn_in
 
-    f = np.dot(L_, np.random.randn(train_length))
+    f = np.dot(L_, np.random.randn(N))
     f_posterior = np.zeros((f.shape[0], n_samples))
     for i in tqdm(range(n_iter)):
 
         sampling = True
         while sampling:
             try:
-                f, _ = elliptical(f, lambda f: log_marginal_likelihood(y_train, f), L_)
+                f, _ = elliptical(f, lambda f: log_marginal_likelihood(y, f), L_)
                 sampling = False
             except IOError:
                 sampling = True
                 # print('Slice sampling shrunk to the current position. Retry sampling ...')
 
         if i >= burn_in:
-            f_posterior[:, i - burn_in] = f
-
-    K_MM = np.zeros((test_length, test_length))
-    for x_idx in range(test_length):
-        for x_prime_idx in range(test_length):
-            K_MM[x_idx, x_prime_idx] = kernel(
-                x_test[x_idx], x_test[x_prime_idx], x_idx == x_prime_idx)
-
-    K_NM = np.zeros((train_length, test_length))
-    for x_idx in range(train_length):
-        for x_prime_idx in range(test_length):
-            K_NM[x_idx, x_prime_idx] = kernel(
-                x_train[x_idx], x_test[x_prime_idx], noise=False)
-
-    n_fs = 10
-    f_samples = np.zeros((test_length, n_fs))
-    for i in range(n_fs):
-        sample_idx = np.random.randint(n_samples)
-        f_n = f_posterior[:, sample_idx]
-        _ = np.einsum("ij,jk,k->i", K_NM.T, K_inv, f_n)
-        var = K_MM - np.einsum("ij,jk,kl->il", K_NM.T, K_inv, K_NM)
-        L_ = np.linalg.cholesky(var)
-        f_m = np.dot(L_, np.random.randn(test_length))
-        f_samples[:, i] = f_m
+            f_posterior[:, i - burn_in] = f * y_std + y_mean
 
     return f_posterior
 
@@ -152,14 +130,36 @@ def elliptical(f, log_likelihood, L):
             theta = np.random.uniform(st, ed)
 
 
+
 if __name__ == "__main__":
     n = 100
-    data_x = np.linspace(0, 4 * np.pi, n)
-    data_y = objective(data_x)
+    x = np.linspace(0, 4 * np.pi, n)
+    y = objective(x)
 
-    x_train, x_test, y_train, y_test = train_test_split(
-        data_x, data_y, test_size=0.30)
+    f_posterior = gpr(x, y, kernel)
 
-    f_posterior = gpr(x_train, y_train, x_test, kernel)
+    plot_gpr(x, y, f_posterior)
 
-    plot_gpr(data_x, data_y, x_train, f_posterior)
+
+    # K_MM = np.zeros((test_length, test_length))
+    # for x_idx in range(test_length):
+    #     for x_prime_idx in range(test_length):
+    #         K_MM[x_idx, x_prime_idx] = kernel(
+    #             x_test[x_idx], x_test[x_prime_idx], x_idx == x_prime_idx)
+
+    # K_NM = np.zeros((train_length, test_length))
+    # for x_idx in range(train_length):
+    #     for x_prime_idx in range(test_length):
+    #         K_NM[x_idx, x_prime_idx] = kernel(
+    #             x_train[x_idx], x_test[x_prime_idx], noise=False)
+
+    # n_fs = 10
+    # f_samples = np.zeros((test_length, n_fs))
+    # for i in range(n_fs):
+    #     sample_idx = np.random.randint(n_samples)
+    #     f_n = f_posterior[:, sample_idx]
+    #     _ = np.einsum("ij,jk,k->i", K_NM.T, K_inv, f_n)
+    #     var = K_MM - np.einsum("ij,jk,kl->il", K_NM.T, K_inv, K_NM)
+    #     L_ = np.linalg.cholesky(var)
+    #     f_m = np.dot(L_, np.random.randn(test_length))
+    #     f_samples[:, i] = f_m
