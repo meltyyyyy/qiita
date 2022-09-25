b4b58ac9570c3c8864cf8c0f9f7765782d59ad41
takeru.abe <takeru.abe@gaiax.com>
2022-09-25 22:34:42 +0900

    update bocs_sa.py

--- bo/bocs/bocs_sa.py
+++ bo/bocs/bocs_sa.py
@@ -154,3 +154,55 @@ class SparseBayesianLinearRegression:
 
         return beta, beta0
 
+def simulated_annealinng(objective, n_vars: np.int64, cooling_rate: np.float64 = 0.985,
+                         n_iter: np.int64 = 100) -> Union[np.ndarray, np.ndarray]:
+    """Run simulated annealing
+    Simulated Annealing (SA) is a probabilistic technique
+    for approximating the global optimum of a given function.
+
+    Args:
+        objective : objective function / statistical model
+        n_vars (np.int64): The number of variables
+        cooling_rate (np.float64, optional): Defaults to 0.985.
+        n_iter (np.int64, optional): Defaults to 100.
+
+    Returns:
+        Union[np.ndarray, np.ndarray]: Best solutions that maximize objective.
+    """
+    X = np.zeros((n_iter, n_vars))
+    obj = np.zeros((n_iter, ))
+
+    # set initial temperature and cooling schedule
+    T = 1.
+    def cool(T): return cooling_rate * T
+
+    curr_x = sample_binary_matrix(1, n_vars)
+    curr_obj = objective(curr_x)
+
+    best_x = curr_x
+    best_obj = curr_obj
+
+    for i in range(n_iter):
+
+        # decrease T according to cooling schedule
+        T = cool(T)
+
+        new_x = sample_binary_matrix(1, n_vars)
+        new_obj = objective(new_x)
+
+        # update current solution
+        if (new_obj > curr_obj) or (rs.rand()
+                                    < np.exp((new_obj - curr_obj) / T)):
+            curr_x = new_x
+            curr_obj = new_obj
+
+        # Update best solution
+        if new_obj > best_obj:
+            best_x = new_x
+            best_obj = new_obj
+
+        # save solution
+        X[i, :] = best_x
+        obj[i] = best_obj
+
+    return X, obj
